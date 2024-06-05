1ffa6736214abd039c2e5b739e3200a8f05c7223
alx <alx@alx.com>
2024-06-05 23:41:38 +0200

    Philosophers task fix

--- src/test/java/ua/com/alxkm/patterns/philosopher/PhilosopherWithSemaphoreTest.java
+++ src/test/java/ua/com/alxkm/patterns/philosopher/PhilosopherWithSemaphoreTest.java
@@ -1,10 +1,21 @@
-package ua.com.alxkm.examples.philosopher;
+package ua.com.alxkm.patterns.philosopher;
 
 import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.Semaphore;
 
 public class PhilosopherWithSemaphoreTest {
+
+    /**
+     * This test method verifies the behavior of the PhilosopherWithSemaphore class, which implements
+     * the Dining Philosophers problem solution using semaphores. It creates a certain number of philosopher
+     * threads (in this case, 5) and initializes a semaphore with the maximum allowed number of concurrent
+     * philosophers minus one (to avoid deadlock). Each philosopher thread represents a philosopher in the
+     * dining philosophers scenario. The test runs the simulation for a specified time duration (e.g., 2 seconds)
+     * to allow the philosophers to attempt to acquire forks and eat. After the simulation period, all philosopher
+     * threads are interrupted to stop the simulation. This test checks whether the Dining Philosophers problem
+     * solution using semaphores works as expected and avoids deadlock and starvation scenarios.
+     */
     @Test
     public void testPhilosophersWithSemaphore() {
         int numOfPhilosophers = 5;
