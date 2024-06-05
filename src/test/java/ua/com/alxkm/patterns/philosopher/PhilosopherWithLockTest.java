07188121029164e73b99a818ef50b3acc801afb2
alx <alx@alx.com>
2024-06-05 23:41:25 +0200

    Philosophers task fix

--- src/test/java/ua/com/alxkm/patterns/philosopher/PhilosopherWithLockTest.java
+++ src/test/java/ua/com/alxkm/patterns/philosopher/PhilosopherWithLockTest.java
@@ -1,4 +1,4 @@
-package ua.com.alxkm.examples.philosopher;
+package ua.com.alxkm.patterns.philosopher;
 
 import org.junit.jupiter.api.Test;
 
@@ -6,6 +6,18 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class PhilosopherWithLockTest {
+
+    /**
+     * This test method verifies the behavior of the PhilosopherWithLock class, which implements
+     * the Dining Philosophers problem solution using locks (ReentrantLocks). It creates a certain
+     * number of philosopher threads (in this case, 5) and initializes an array of locks to represent
+     * the forks on the table. Each philosopher thread represents a philosopher in the dining philosophers
+     * scenario and is initialized with the corresponding left and right forks. The test runs the simulation
+     * for a specified time duration (e.g., 5 seconds) to allow the philosophers to attempt to acquire forks
+     * and eat. After the simulation period, all philosopher threads are interrupted to stop the simulation.
+     * This test checks whether the Dining Philosophers problem solution using locks works as expected and
+     * avoids deadlock and starvation scenarios.
+     */
     @Test
     public void testDiningPhilosophers() {
         int numOfPhilosophers = 5;
