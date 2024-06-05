988de81165ef6f1eaf89a8418a3bc6845bc87021
alx <alx@alx.com>
2024-06-05 23:32:00 +0200

    Syncronizers examples

--- src/main/java/ua/com/alxkm/patterns/synchronizers/BarrierExample.java
+++ src/main/java/ua/com/alxkm/patterns/synchronizers/BarrierExample.java
@@ -1,12 +1,22 @@
-package ua.com.alxkm.patterns.barrier;
+package ua.com.alxkm.patterns.synchronizers;
 
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 
+/**
+ * The BarrierExample class demonstrates the usage of the CyclicBarrier class to synchronize multiple threads
+ * at a certain point, ensuring they all reach that point before proceeding.
+ */
 public class BarrierExample {
     private static final int THREAD_COUNT = 3;
     private static final CyclicBarrier BARRIER = new CyclicBarrier(THREAD_COUNT, () -> System.out.println("All threads have reached the barrier, continuing..."));
 
+    /**
+     * The main method starts multiple threads and demonstrates how they synchronize at the barrier.
+     *
+     * @param args The command line arguments (not used in this example).
+     * @throws InterruptedException If any thread is interrupted while waiting.
+     */
     public static void main(String[] args) throws InterruptedException {
         Runnable task = () -> {
             System.out.println(Thread.currentThread().getName() + " started");
