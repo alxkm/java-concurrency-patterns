36e9be041fd5d668be8f85b1e969e85b9eaeb534
alx <alx@alx.com>
2024-06-06 00:01:27 +0200

    Classes moving

--- src/main/java/ua/com/alxkm/patterns/reentrantlock/ReentrantLockCounter.java
+++ src/main/java/ua/com/alxkm/patterns/reentrantlock/ReentrantLockCounter.java
@@ -2,20 +2,33 @@ package ua.com.alxkm.patterns.reentrantlock;
 
 import java.util.concurrent.locks.ReentrantLock;
 
+/**
+ * A simple counter implementation using a ReentrantLock for synchronization.
+ * The ReentrantLockCounter class provides methods for incrementing and
+ * retrieving a counter value with exclusive locking.
+ */
 public class ReentrantLockCounter {
-    private final ReentrantLock lock = new ReentrantLock();
-    private int counter;
+    private final ReentrantLock lock = new ReentrantLock(); // The ReentrantLock for synchronization
+    private int counter; // The shared counter value
 
+    /**
+     * Increments the counter value with exclusive locking.
+     */
     public void incrementCounter() {
-        lock.lock();
+        lock.lock(); // Acquire the lock
         try {
-            counter++;
+            counter++; // Increment the counter
         } finally {
-            lock.unlock();
+            lock.unlock(); // Release the lock
         }
     }
 
+    /**
+     * Retrieves the current value of the counter.
+     *
+     * @return The current value of the counter.
+     */
     public int getCounter() {
-        return counter;
+        return counter; // Return the current value of the counter
     }
 }
