88de1f4a4ff3e65a3c876b95465c5f67f7b2c626
alx <alx@alx.com>
2024-06-07 00:30:40 +0200

    The MultithreadedContext class update

--- src/main/java/ua/com/alxkm/patterns/multithreadedcontext/MultithreadedContext.java
+++ src/main/java/ua/com/alxkm/patterns/multithreadedcontext/MultithreadedContext.java
@@ -3,24 +3,32 @@ package ua.com.alxkm.patterns.multithreadedcontext;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-public class MultithreadedContext {
-    private static volatile MultithreadedContext instance;
+/**
+ * The MultithreadedContext class provides a thread-safe context for executing operations.
+ */
+public final class MultithreadedContext {
+    private static final MultithreadedContext INSTANCE = new MultithreadedContext();
     private final Lock lock = new ReentrantLock();
 
-    private MultithreadedContext() {
-    }
+    /**
+     * Private constructor to prevent instantiation.
+     */
+    private MultithreadedContext() {}
 
+    /**
+     * Returns the singleton instance of MultithreadedContext.
+     *
+     * @return the singleton instance
+     */
     public static MultithreadedContext getInstance() {
-        if (instance == null) {
-            synchronized (MultithreadedContext.class) {
-                if (instance == null) {
-                    instance = new MultithreadedContext();
-                }
-            }
-        }
-        return instance;
+        return INSTANCE;
     }
 
+    /**
+     * Executes a Runnable operation in a thread-safe context.
+     *
+     * @param runnable the operation to be executed
+     */
     public void apply(Runnable runnable) {
         lock.lock();
         try {
@@ -30,13 +38,37 @@ public class MultithreadedContext {
         }
     }
 
-    public void tryApply(Runnable runnable) {
+    /**
+     * Attempts to execute a Runnable operation in a thread-safe context without blocking.
+     * If the lock is not available, the operation will not be executed.
+     *
+     * @param runnable the operation to be executed
+     * @return true if the operation was executed successfully, false otherwise
+     */
+    public boolean tryApply(Runnable runnable) {
         if (lock.tryLock()) {
             try {
                 runnable.run();
+                return true;
             } finally {
                 lock.unlock();
             }
         }
+        return false;
+    }
+
+    /**
+     * Main method demonstrating the usage of MultithreadedContext.
+     *
+     * @param args command-line arguments (not used)
+     */
+    public static void main(String[] args) {
+        MultithreadedContext context = MultithreadedContext.getInstance();
+
+        Runnable operation = () -> {
+            System.out.println("Thread-safe operation is performed.");
+        };
+
+        context.apply(operation);
     }
 }
