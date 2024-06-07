34bf7104181bc7234ffca808941d90ee593689c5
alx <alx@alx.com>
2024-06-07 23:09:33 +0200

    Future example

---
+++ src/test/java/org/alxkm/patterns/future/FutureExampleTest.java
@@ -0,0 +1,30 @@
+package org.alxkm.patterns.future;
+
+import org.alxkm.patterns.future.Task;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FutureExampleTest {
+
+    /**
+     * This test method verifies the functionality of the FutureResult pattern, which allows
+     * asynchronous execution of tasks and retrieval of their results. It creates a single-threaded
+     * executor service and submits a task for execution. The test then waits for the task to complete
+     * and retrieves the result using the Future object. Finally, the test asserts that the result obtained
+     * from the Future matches the expected value (123) and shuts down the executor service.
+     */
+    @Test
+    public void testFutureResult() throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        Future<Integer> future = executorService.submit(new Task());
+
+        assertEquals(123, future.get());
+        executorService.shutdown();
+    }
+}
