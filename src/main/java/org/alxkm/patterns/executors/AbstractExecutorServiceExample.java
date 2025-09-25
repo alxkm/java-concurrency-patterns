6983889b0ac18b91fb73f83d70d7d9d8ef66a0a2
Oleksandr Klymenko <alexanderklmn@gmail.com>
2025-09-25 20:22:01 +0200

    Fix imports (#77)

--- src/main/java/org/alxkm/patterns/executors/AbstractExecutorServiceExample.java
+++ src/main/java/org/alxkm/patterns/executors/AbstractExecutorServiceExample.java
@@ -1,8 +1,15 @@
 package org.alxkm.patterns.executors;
 
-import java.util.concurrent.*;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.concurrent.AbstractExecutorService;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
--- src/main/java/org/alxkm/patterns/executors/CompletionServiceExample.java
+++ src/main/java/org/alxkm/patterns/executors/CompletionServiceExample.java
@@ -2,7 +2,13 @@ package org.alxkm.patterns.executors;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 /**
  * Demonstrates the usage of ExecutorCompletionService for managing and retrieving
